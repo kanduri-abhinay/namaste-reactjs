b883504ba684e211c448984eade81393fcae1cfc
Kanduri Abhinay <kanduriabhinay@Kanduris-MacBook-Air.local>
2023-01-02 17:56:51 +0530

    babel plugin to remove console logs

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const element = React.createElement("h1", {}, "hello abhinay");
-// console.log(React, element);
+console.log(React, element);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
 const element2 = React.createElement("h1", {}, "hello footer");
-// console.log(React, element);
+console.log(React, element);
 const footer = ReactDOM.createRoot(document.getElementById("footer"));
 footer.render(element2);
