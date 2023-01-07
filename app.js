8d9e89d544e99157aeb3d212ac8fac82f32931bb
Kanduri Abhinay <kanduriabhinay@Kanduris-MacBook-Air.local>
2023-01-07 12:41:05 +0530

    initial header

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
+
+const Title = () => {
+  return (
+    <img
+      alt="logo"
+      className="logo"
+      src={"https://foodvilla.no/src/img/logo.png"}
+    />
+  );
+};
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const Body = () => {
+  return <h1>body</h1>;
+};
+const Footer = () => {
+  return <h1>Footer</h1>;
+};
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <Body />
+      <Footer />
     </Fragment>
   );
 };
