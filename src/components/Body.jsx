7b36f6388f113bb319edc2dc1928067b19cfba7e
Kanduri Abhinay <kanduriabhinay@Kanduris-MacBook-Air.local>
2023-01-10 09:37:40 +0530

    search

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
+const filterData = (searchText, data) => {
+  return data.filter((item) => item.data.name.toLowerCase().includes(searchText.toLowerCase()));
+};
 const Body = () => {
+  const [searchText, updateSearchText] = useState("");
+  const [restaurants, updateRestaurants] = useState(restaurantList);
   return (
-    <div className="cards-container">
-      {restaurantList.map((restaurant) => {
-        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchText}
+          className="search-input"
+          onChange={(e) => {
+            updateSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={(e) => {
+            updateRestaurants(filterData(searchText, restaurantList));
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="cards-container">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default Body;
