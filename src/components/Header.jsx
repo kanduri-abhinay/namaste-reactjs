91009cfece72b596136863d41ffb8b99e0428c98
Kanduri Abhinay <kanduriabhinay@gmail.com>
2023-01-21 10:38:51 +0530

    login/logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
+import { useState } from "react";
+
 const Title = () => {
-    return (
-      <img
-        alt="logo"
-        className="logo"
-        src={"https://foodvilla.no/src/img/logo.png"}
-      />
-    );
-  };
-  const Header = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  return (
+    <img
+      alt="logo"
+      className="logo"
+      src={"https://foodvilla.no/src/img/logo.png"}
+    />
+  );
+};
+const Header = () => {
+  const [isLoggedIn,updateLoggedIn]=useState(false);
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
       </div>
-    );
-  };
-  export default Header;
+      {!isLoggedIn ? (
+        <button onClick={() => updateLoggedIn(true)}>LogIn</button>
+      ) : (
+        <button onClick={() => updateLoggedIn(false)}>LogOut</button>
+      )}
+    </div>
+  );
+};
+export default Header;
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -3,6 +3,7 @@ export default function Shimmer() {
   for (let i = 0; i < 50; i++) {
     boxUi.push(
       <div
+      key={i}
         className="card"
         style={{ height: "200px", width: "200px", background: "#d1e0e0" }}
       ></div>
